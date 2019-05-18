f0f3b8b99c3c9a172296cb877f673bac2b02bdcc
xenteros <adan@gurgul.com.pl>
2019-05-18 15:40:01 +0200

    Test currency exchange algorithm

--- src/main/java/com/github/xenteros/Main.java
+++ src/main/java/com/github/xenteros/Main.java
@@ -6,6 +6,15 @@ class Main {
 
     public static void main(String[] args) {
 
+        for (String arg : args) {
+            try {
+                int amount = Integer.parseInt(arg);
+                System.out.println("Giving change for " + arg);
+                printExchange(amount);
+            } catch (RuntimeException e ) {
+                System.out.println("Something went wrong!");
+            }
+        }
 
     }
 
